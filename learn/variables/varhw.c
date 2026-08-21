ff841d2b6a1f8015be448383ee463f32d254f7c1
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-08-21 19:10:00 -0400

    Added: var homework in C

---
+++ learn/variables/varhw.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/*
+ * Homework: make an int, float, double, char, char[], bool
+ */
+
+int main() {
+
+    int age = 19;
+    float gpa = 3.6;
+    double longNumber = 983219283498;
+    char symbol = '!';
+    char name[] = "Andre";
+    bool isStudent = true;
+
+    printf("Your age is: %d\n", age);
+    printf("Your gpa is: %.1f\n", gpa);
+    printf("A very long number is: %.15lf\n", longNumber);
+    printf("A symbol is %c\n", symbol);
+    printf("Your name is: %s\n", name);
+    printf("You are a student %b\n: ", isStudent);
+}
