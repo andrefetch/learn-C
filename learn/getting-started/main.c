d654d4bb5453ffdae2cd9e3a357363c529365229
Andre Nunes da Silva <andresilvapersonal2007@gmail.com>
2026-08-21 17:28:42 -0400

    feat: first program in C

---
+++ learn/getting-started/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    // Comments are written with two slashes.
+
+    /*
+     Multi Line comments!
+     This is super awesome!
+     */
+
+    printf("Hello, this is my first C program!\n");
+    printf("It's really good!\n");
+
+    // Assignment: Two comments, two print statements
+
+    // Comment One: Learning C seems awesome!
+
+    /*
+     * I like these multiline comments
+     */
+
+    printf("I'm ready to manage my own memory.\n");
+    printf("Structs and structs!\n");
+
+    return 0; // Non Zero returns means an error (like exit code 1)
+}
